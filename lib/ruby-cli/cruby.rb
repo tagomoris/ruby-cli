456b1f2bdfe77b40696ab5d5db6863eac3c2cc92
TAGOMORI Satoshi <tagomoris@gmail.com>
2016-09-15 18:20:48 +0900

    fix to exclude mruby related files from rubygem

--- lib/ruby-cli/cruby.rb
+++ lib/ruby-cli/cruby.rb
@@ -25,11 +25,39 @@ module RubyCLI
         end
 
         Dir.chdir project_root_dir
+        inject_to_gemspec(name)
         inject_to_rakefile
         FileUtils.rm_rf cruby_dir_tmp
       ensure
         Dir.chdir original_working_dir
       end
+    end
+
+    def self.inject_to_gemspec(name)
+      content_header = <<-EOF
+MRUBY_CLI_FILES = %w(Dockerfile bintest build_config.rb docker-compose.yml mrbgem.rake mrblib tools)
+
+EOF
+      reject_mruby_files = '.reject{|f| MRUBY_CLI_FILES.any?{|s| f.start_with?(s) } }'
+
+      gemspec_filename = "#{name}.gemspec"
+      gemspec_tmp_filename = "#{gemspec_filename}.tmp"
+      File.open(gemspec_tmp_filename, "a") do |f|
+        File.open(gemspec_filename) do |r|
+          r.readlines.each do |line|
+            if line.start_with?("Gem::Specification")
+              f.puts content_header
+              f.write line
+            elsif line =~ /spec\.files/
+              f.puts line.chomp + reject_mruby_files
+            else
+              f.write line
+            end
+          end
+        end
+      end
+      File.delete(gemspec_filename)
+      File.rename(gemspec_tmp_filename, gemspec_filename)
     end
 
     def self.inject_to_rakefile
