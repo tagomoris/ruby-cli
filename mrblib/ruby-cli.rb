9463b5af24bf3ea47d1118d15c66a34377b06c40
TAGOMORI Satoshi <tagomoris@gmail.com>
2016-09-15 16:55:34 +0900

    add command line options

--- mrblib/ruby-cli.rb
+++ mrblib/ruby-cli.rb
@@ -1,6 +1,8 @@
 def __main__(argv)
-  if argv[1] == "version" || argv[1] == "-v"
+  if argv[1] == "-v"
     puts "v#{RubyCLI::VERSION}"
+  elsif argv[1] == "-h"
+    puts "ruby-cli PROJECT_NAME"
   else
     RubyCLI::Setup.execute(argv[1])
   end
